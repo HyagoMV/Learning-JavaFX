e2b236f75da857edfd14e202933db6ad56042f7f
Hyago M. Vale <hyagomvale@gmail.com>
2022-01-04 13:49:39 -0300

    Base

---
+++ src/test/java/com/github/hyagomv/MainTest.java
@@ -0,0 +1,13 @@
+package com.github.hyagomv;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+	@Test
+	void test() {
+		assertTrue(true);
+	}
+}
