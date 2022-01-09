8440d7e21f0b5baae1a6837bc4057777c9f12e89
Hyago M. Vale <hyagomvale@gmail.com>
2022-01-09 14:31:15 -0300

    Update HomeController.java

--- src/main/java/com/github/hyagomv/application/controller/HomeController.java
+++ src/main/java/com/github/hyagomv/application/controller/HomeController.java
@@ -8,7 +8,16 @@ import javafx.scene.control.Button;
 public class HomeController {
 
 	@FXML
-	private Button btnAbout;
+	private Button aboutButton;
+	
+	@FXML
+	private void initialize() {
+		previousButton.setOnAction(this::onAboutButtonActionEventHandler);
+	}
+	
+	private void onAboutButtonActionEventHandler(ActionEvent event) {
+		System.out.println("About");
+	}
 
 	public void initialize() {
 		btnAbout.setOnAction(event -> {
